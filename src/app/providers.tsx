4b4e896ca34de2c76f3da014b275f727020b5f3b
Mike S <mksoofian@gmail.com>
2024-07-09 16:11:35 -0400

    created form context and provider for form

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,32 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { createContext, useContext, useState } from "react";
+
+export type FormContextType = {
+  query: string;
+  setQuery: (_value: string) => void;
+  category: string;
+  setCategory: (_value: string) => void;
+};
+export const FormContext = createContext<FormContextType>({
+  query: "",
+  setQuery: (_value: string) => {},
+  category: "",
+  setCategory: (_value: string) => {},
+});
+
+export const useFormContext = () => useContext(FormContext);
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  const [query, setQuery] = useState("");
+  const [category, setCategory] = useState("");
+
+  return (
+    <ChakraProvider>
+      <FormContext.Provider value={{ query, setQuery, category, setCategory }}>
+        {children}
+      </FormContext.Provider>
+    </ChakraProvider>
+  );
 }
--- src/comoponents/form.tsx
+++ src/comoponents/form.tsx
@@ -1,23 +1,45 @@
+"use client";
+
+import { useFormContext } from "@/app/providers";
 import {
   FormControl,
   FormLabel,
   Input,
-  FormHelperText,
   Select,
-  Flex,
   VStack,
 } from "@chakra-ui/react";
 
 export default function SearchForm() {
+  const { query, setQuery, category, setCategory } = useFormContext();
+
+  function handleSearch(term: string) {
+    setQuery(term);
+  }
+
+  function handleCategorySelection(category: string) {
+    setCategory(category);
+  }
+
   return (
     <VStack spacing={4} align="stretch" padding={5}>
       <FormControl>
         <FormLabel>Search Database</FormLabel>
-        <Input type="text" />
+        <Input
+          type="text"
+          value={query}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>Categories</FormLabel>
-        <Select placeholder="Select Category">
+        <Select
+          placeholder="Select Category"
+          onChange={(e) => {
+            handleCategorySelection(e.target.value);
+          }}
+        >
           <option value="films">Films</option>
           <option value="people">People</option>
           <option value="planets">Planets</option>
